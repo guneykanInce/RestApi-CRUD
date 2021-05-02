1ba4c37921f8c35f758ad3f75f38430131214e9a
Güneykan İnce <U088470@kfs.local>
2021-05-03 00:03:04 +0300

    ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# RestApi-CRUD
-CRUD operations with RestApi and RobotFramework BDD, JUNIT unit test, Docker File, CI with GitHub Actions
+# User Account Management API
+CRUD operations with RestApi services and RobotFramework for BDD, JUNIT5 for unit test, Docker, CI with GitHub Actions
+
+## Requirements
+
+* Java 8+
+* Maven 4+
+
+## Tech Stack
+
+* [Spring Boot](https://projects.spring.io/spring-boot/)
+* [H2 Database](https://www.h2database.com/html/main.html)
+* [Junit5](https://junit.org/junit5/)
+* [Robot Framework](https://robotframework.org/)
+
+## Setup 
+
+Clone the repository from git
+
+```bash
+git clone https://github.com/guneykanInce/RestApi-CRUD.git 
+```
+Download  libraries  with Maven
+
+```bash
+mvn clean install
+```
+
+## Run
+
+```bash
+mvn spring-boot:run
+```
+Go http://localhost:8080/
+
+
--- src/test/java/com/project/accounts/controller/UserAccountControllerTest.java
+++ src/test/java/com/project/accounts/controller/UserAccountControllerTest.java
@@ -1,33 +1,171 @@
 package com.project.accounts.controller;
 
+import com.project.accounts.dto.UserAccountRequestDto;
+import com.project.accounts.dto.UserAccountResponseDto;
+import com.project.accounts.model.UserAccountEntity;
+import com.project.accounts.repository.IUserAccountRepository;
 import com.project.accounts.service.IUserAccountServices;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
 class UserAccountControllerTest {
 
     @Mock
     IUserAccountServices userAccountServices;
+    
+    @Mock
+    ModelMapper modelMapper;
+    
+    @Mock
+    IUserAccountRepository accountsRepository;
 
-
+    @InjectMocks
+    UserAccountController userAccountController;
+    
     @Test
     void getAllAccounts() {
-
+    	MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        
+        UserAccountEntity entity = UserAccountEntity.builder()
+                .id(1L)
+                .address("Merdivenkoy")
+                .country("TR")
+                .department("IT")
+                .email("test@test.com")
+                .name("Guneykan")
+                .phone("99999999999").build();
+        
+        when(userAccountServices.getAllUserAccounts()).thenReturn(asList(entity));
+        
+		ResponseEntity<List<UserAccountResponseDto>> responseEntityList = userAccountController.getAllAccounts();
+         
+        assertEquals(responseEntityList.getStatusCodeValue(),200);
     }
 
     @Test
     void findAccountById() {
+    	MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        
+        UserAccountEntity entity = UserAccountEntity.builder()
+                .id(1L)
+                .address("Merdivenkoy")
+                .country("TR")
+                .department("IT")
+                .email("test@test.com")
+                .name("Guneykan")
+                .phone("99999999999").build();
+        
+        when(userAccountServices.findById(entity.getId())).thenReturn(Optional.of(entity));
+        
+		ResponseEntity<UserAccountResponseDto> responseEntity = userAccountController.findAccountById(entity.getId());
+         
+        assertEquals(responseEntity.getStatusCodeValue(),200);
     }
 
     @Test
     void createAccount() {
-    }
+    	MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        
+        UserAccountEntity entity = UserAccountEntity.builder()
+                .id(1L)
+                .address("Merdivenkoy")
+                .country("TR")
+                .department("IT")
+                .email("test@test.com")
+                .name("Guneykan")
+                .phone("99999999999").build();
+        
+        when(userAccountServices.createAccount(any(UserAccountEntity.class))).thenReturn(entity);
+         
+        UserAccountRequestDto requestEntity = UserAccountRequestDto.builder().id(1L)
+                .address("Merdivenkoy")
+                .country("TR")
+                .department("IT")
+                .email("test@test.com")
+                .name("Guneykan")
+                .phone("99999999999").build();
 
-    @Test
-    void deleteAccount() {
+        when(modelMapper.map(requestEntity, UserAccountEntity.class)).thenReturn(entity);
+        
+		@SuppressWarnings("unchecked")
+		ResponseEntity<Object> responseEntity = userAccountController.createAccount(requestEntity);
+         
+        assertEquals(responseEntity.getStatusCodeValue(),201);
+        assertEquals(responseEntity.getHeaders().getLocation().getPath(),"/1");
     }
 
     @Test
     void updateAccount() {
+    	MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        
+        UserAccountEntity entity = UserAccountEntity.builder()
+                .id(1L)
+                .address("Merdivenkoy")
+                .country("TR")
+                .department("IT")
+                .email("test@update.com")
+                .name("Guneykan")
+                .phone("99999999999").build();
+        
+        when(userAccountServices.updateAccount(any(UserAccountEntity.class))).thenReturn(entity);
+         
+        UserAccountRequestDto requestEntity = UserAccountRequestDto.builder().id(1L)
+                .address("Merdivenkoy")
+                .country("TR")
+                .department("IT")
+                .email("test@update.com")
+                .name("Guneykan")
+                .phone("99999999999").build();
+
+        when(modelMapper.map(requestEntity, UserAccountEntity.class)).thenReturn(entity);
+        
+		@SuppressWarnings("unchecked")
+		ResponseEntity<Object> responseEntity = userAccountController.updateAccount(requestEntity);
+         
+        assertEquals(responseEntity.getStatusCodeValue(),201);
+        assertEquals(responseEntity.getHeaders().getLocation().getPath(),"/1");
+    }
+    
+    @Test
+    void deleteAccount() {
+    	MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        
+        UserAccountEntity entity = UserAccountEntity.builder()
+                .id(1L)
+                .address("Merdivenkoy")
+                .country("TR")
+                .department("IT")
+                .email("test@test.com")
+                .name("Guneykan")
+                .phone("99999999999").build();
+        
+        when(userAccountServices.findById(entity.getId())).thenReturn(Optional.of(entity));
+        
+		@SuppressWarnings("unchecked")
+		ResponseEntity<Object> responseEntity = userAccountController.deleteAccount(entity.getId());
+         
+        assertEquals(responseEntity.getStatusCodeValue(),200);
     }
 }
